1d9f215af5ed7207efbbcdb4e8b4f423639be1e5
Lucas Risaro <lucasrisaro@gmail.com>
2020-06-02 20:30:30 -0300

    Agrego codigo de ejercicio 10 terminado

--- ejercicio_10/infec_simulation.py
+++ ejercicio_10/infec_simulation.py
@@ -7,31 +7,46 @@ class infec_simulation(Simulation):
 
     ParticleClass = infec_particles
 
-    def __init__(self, amount_of_circles=10, max_x=30, max_y=30, frames=100, fps=10):
+    def __init__(self, amount_of_circles=10, max_x=30, max_y=30, frames=100, fps=10, model = "A", type_of_moving = 1):
         self.particles = []
         self.max_x = max_x
         self.max_y = max_y
         self.amount_of_circles = amount_of_circles
+        self.model = model
+        self.type_of_moving = type_of_moving
         self.init_particles(amount_of_circles)
         self.movements = 0
-        self.frames = frames
         self.fps = fps
         self.left_density = []
         self.right_density = []
         self.area = self.max_x * self.max_y * 4
-        self.infections_per_cicle = [5] #5 infected at timestamp 1 always
+        self.infections_per_cicle = []
+        self.frames = frames
+
 
     def init_particles(self, amount_of_circles):
         inicial_infected_particles = 0
+        moving_particles = 0
         for i in range(0, amount_of_circles):
+            color = "Blue"
+            can_move = 1
 
             if (inicial_infected_particles < 5): #95% of the particles began infected
                 number = random.uniform(0,1)
+
                 if (number < 0.5): #50% of probability to began infected
-                    self.place_particle("Red")
+                    color = "Red"
                     inicial_infected_particles += 1
-            else:
-                self.place_particle("Blue")
+
+            if(self.type_of_moving == 3):  # only 50% of the particles can move
+                if (moving_particles <= (amount_of_circles / 2)):
+                    can_move = 0
+                    moving_particles += 1
+
+            if (self.type_of_moving == 2 and color == "Red"): #Only healthy particles can move
+                can_move = 0
+
+            self.place_particle(color, can_move)
 
     def init(self):
         self.circles = []
@@ -39,11 +54,11 @@ class infec_simulation(Simulation):
             self.circles.append(particle.draw(self.ax))
         return self.circles
 
-    def place_particle(self, color):
+    def place_particle(self, color, can_move):
         x = random.randint(-self.max_x + 1, self.max_x - 1)
         y = random.randint(-self.max_y + 1, self.max_y - 1)
 
-        particle = self.ParticleClass(x, y, self.max_x, self.max_y, color)
+        particle = self.ParticleClass(x, y, self.max_x, self.max_y, color, can_move)
 
         if(color == "Red"):
             particle.time_infected +=1
@@ -62,10 +77,14 @@ class infec_simulation(Simulation):
 
     def advance_animation(self):
         for i, p in enumerate(self.particles):
-            p.advance()
+
+            if(p.can_move == 1):
+                p.advance()
+
             self.circles[i].center = p.r
 
-        #self.cure_particles(self.circles) #TO USE MODEL A COMMENT THIS LINE
+        if(self.model == "B"):
+            self.cure_particles(self.circles)
 
         self.change_state(self.circles)
         self.count_infections(self.circles)
@@ -114,7 +133,7 @@ class infec_simulation(Simulation):
         else:
             return False
 
-    #Count the number of infections per cicle
+    #Count the number of infections per cicle and stop them if type of moving is 2
     def count_infections(self, circles):
 
         number_of_infected = 0
@@ -125,18 +144,33 @@ class infec_simulation(Simulation):
                 number_of_infected = number_of_infected + 1
                 self.particles[i].time_infected += 1
 
+                if(self.type_of_moving == 2): #Only healthy particles can move
+
+                    if (self.particles[i].time_infected >= 10 or self.particles[i].time_infected <= 20):
+
+                        prob_to_stop = random.uniform(0,1)
+
+                        if(prob_to_stop <= 0,5):
+                            self.particles[i].can_move = 0
+
+                    if (self.particles[i].time_infected > 20):
+                        self.particles[i].can_move = 0
+
         self.infections_per_cicle.append(number_of_infected)
 
 
-    #Cure the particles if it can be cure
+    #Cure the particles if it can be cure and let them move if type moving is 2
     def cure_particles(self, circles):
 
         for i in range(0, len(self.particles)):
 
             if(self.particles[i].time_infected >= 20):
 
-                prob_cure = random.uniform(0,3)
+                prob_cure = random.uniform(0,1)
 
-                if(prob_cure <= 0,1):
+                if(prob_cure <= 0,8):
                     self.circles[i].set_facecolor("Blue")
                     self.particles[i].time_infected = 0
+
+                    if(self.type_of_moving == 2): #Only healthy particles can move
+                        self.particles[i].can_move = 1
